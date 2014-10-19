e9e9a0292bcf44f48bca4058e98e23e37bedb22b
AlexanderGodbout <aggodbout@ucdavis.edu>
2014-10-18 18:41:02 -0700

    Create database_v2.py

---
+++ database_v2.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python
+
+
+import glob 
+import urllib2
+
+b='burrito'
+		
+def organisms(b):
+	if b == 'burrito':
+		return 'wheat.fasta_soybean.fasta'	
+
+def database(org):
+	c = org.split("_")
+	for i in range(0,len(c)):
+		html='https://raw.githubusercontent.com/tparkeressig/ProFacts/master'
+		file=c[i]
+		html='https://raw.githubusercontent.com/tparkeressig/ProFacts/master/'
+		website = html + file 
+		response = urllib2.urlopen(website)
+		headers = response.info()
+		data = response.read()
+		print data[0]
+		fo = open("result.txt","a")
+		fo.write(data)
+		fo.close
+		
+	
+org = organisms(b)
+database(org)
